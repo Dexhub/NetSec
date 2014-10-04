99ae70f41226138161dd25bd17248c5ee9954529
Himanshu Shah <Himanshu1410@live.com>
2014-10-04 01:22:13 -0400

    Removing duplicate code

--- tcpechotimecli.c
+++ tcpechotimecli.c
@@ -61,7 +61,7 @@ int main(int argc, char *argv[])
             continue;
         }
 
-        if(option == 1) // Echo client selected
+        if(option == 1 || option == 2) // Echo client selected
         {
             int pfd[2];
             char buf[SIZE];
@@ -81,8 +81,13 @@ int main(int argc, char *argv[])
             {
                 /* child */
                 close(pfd[0]);
+               
                 int n = sprintf(pfd_buffer, "%d",pfd[1]); 
-                execlp("xterm","xterm", "-e","./echo_cli", inet_ntoa(*addr_list[0]), pfd_buffer, (char*)0);
+                if (option == 1)
+                    execlp("xterm","xterm", "-e","./echo_cli", inet_ntoa(*addr_list[0]), pfd_buffer, (char*)0);
+                else
+                    execlp("xterm","xterm", "-e","./time_cli", inet_ntoa(*addr_list[0]), pfd_buffer, (char*)0);
+                
                 close(pfd[1]);
 
             }
@@ -119,63 +124,6 @@ int main(int argc, char *argv[])
             }
 
         } //Echo server finish
-        else if (option == 2)// daytime client selected
-        {
-            int pfd[2];
-            char buf[SIZE];
-            int pid, nread;
-            char pfd_buffer[10];
-            if (pipe(pfd) == -1)
-            {
-              perror("Time Error: pipe failed\n");
-              exit(1);
-            }
-            if ((pid = fork()) < 0)
-            {
-              perror("Time Error: fork failed\n");
-              exit(2);
-            }
-            if (pid == 0)// Child process
-            {
-                /* child */
-                close(pfd[0]);
-                int n = sprintf(pfd_buffer, "%d",pfd[1]); 
-                execlp("xterm","xterm", "-e","./time_cli", inet_ntoa(*addr_list[0]), pfd_buffer, (char*)0);
-                close(pfd[1]);
-
-            }
-            else // Parent Process code
-            {
-                close(pfd[1]);
-
-                //Select on pfd[0] and stdin to see if someone has typed on main terminal
-                fd_set rset;
-                int maxfdp1;
-                FD_ZERO(&rset); 
-                while(1) 
-                {
-                    FD_SET(STDIN_FILENO, &rset);// Stdin
-                    FD_SET(pfd[0], &rset); // Pipe
-                    maxfdp1 = max(STDIN_FILENO, pfd[0]) + 1;
-                    select(maxfdp1, &rset, NULL, NULL, NULL);
-                    if (FD_ISSET(STDIN_FILENO, &rset))  /* socket is readable */
-                    {
-                        char sendline[MAXLINE+1];
-                        char *temp = fgets(sendline, MAXLINE, stdin);
-                        printf("\n Please use xterm to enter input\n");
-                    }
-                    else if(FD_ISSET(pfd[0], &rset))
-                    {
-                        while ((nread = read(pfd[0], buf, SIZE)) != 0)
-                            printf("CHILD SIGNAL: %s\n", buf);
-                        wait(NULL);
-                        break;
-                    }
-                 }
-                 close(pfd[0]);
-                // Read from the pipe
-            }
-         } // Time finish
         else if (option == 3)// daytime client selected
         {
             printf("Exiting Program\n");
